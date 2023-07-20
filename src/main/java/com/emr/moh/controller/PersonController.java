208227a5fe4f8255bbb47537c10f366b0117007d
Frank Mwesigwa <frank.mwesigwa1@gmail.com>
2023-07-20 17:30:53 +0300

    fix posting error

--- src/main/java/com/emr/moh/controller/PersonController.java
+++ src/main/java/com/emr/moh/controller/PersonController.java
@@ -3,12 +3,14 @@ package com.emr.moh.controller;
 import java.util.UUID;
 
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
 import org.hl7.fhir.r4.model.ContactPoint;
 import org.hl7.fhir.r4.model.Enumerations;
 import org.hl7.fhir.r4.model.Patient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.emr.moh.modal.Person;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.model.primitive.IdDt;
+import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
-import lombok.val;
 
 @RestController
 @RequestMapping("/api")
@@ -30,19 +31,20 @@ public class PersonController {
     public ResponseEntity<?> addPerson(@RequestBody Person person) {
 
         FhirContext ctx = FhirContext.forR4();
-        String serverBase = "http://localhost:8080/fhir/Patient";
+        String serverBase = "http://localhost:8080/fhir";
         IGenericClient client = ctx.newRestfulGenericClient(serverBase);
 
         Patient patient = new Patient();
 
-        patient.addIdentifier()
-                .setSystem("http://acme.org/mrns")
-                .setValue(UUID.randomUUID().toString());
+        patient.setId(UUID.randomUUID().toString());
+        patient.addIdentifier().setSystem("http://acme.com/MRNs").setValue("frank-emr");
+        patient.setActive(true);
+        patient.setBirthDate(person.getBirthDate());
 
         patient.addName()
                 .setText(person.getFname())
                 .addGiven(person.getLname())
-                .addGiven("Jonah");
+                .addGiven(person.getOtherNames());
 
         patient.addTelecom().setSystem(ContactPoint.ContactPointSystem.PHONE).setValue(person.getMobileNo());
         patient.addTelecom().setSystem(ContactPoint.ContactPointSystem.EMAIL).setValue(person.getEmail());
@@ -51,7 +53,7 @@ public class PersonController {
                 .setCity(person.getCity())
                 .setPostalCode(person.getPostalCode());
 
-        patient.getManagingOrganization().setReference("http://example.com/base/Organization/FOO");
+        // patient.getManagingOrganization().setReference("http://example.com/base/Organization/FOO");
 
         switch (person.getGender()) {
             case "male" -> patient.setGender(Enumerations.AdministrativeGender.MALE);
@@ -63,19 +65,12 @@ public class PersonController {
         System.out.println(encoded);
 
         MethodOutcome outcome = client.create()
-                .resource(encoded)
+                .resource(patient)
                 .prettyPrint()
                 .encodedJson()
                 .execute();
 
-        // IdDt id = (IdDt) outcome.getId();
-        System.out.println(outcome);
-
-        // patient.setId(outcome.getResource().getIdElement().getIdPartAsLong().toString());
-
-        // newPerson.setEmail(person.getEmail());
-        // newPerson.setFname(person.getFname());
-        // newPerson.setLname(person.getLname());
+        System.out.println(patient);
         return new ResponseEntity<>(outcome.getCreated(), HttpStatus.CREATED);
     }
 
@@ -83,7 +78,7 @@ public class PersonController {
     public ResponseEntity<?> getPatient() {
 
         FhirContext ctx = FhirContext.forR4();
-        String serverBase = "http://localhost:8080/fhir/Patient";
+        String serverBase = "http://localhost:8080/fhir";
         IGenericClient client = ctx.newRestfulGenericClient(serverBase);
 
         try {
@@ -100,6 +95,56 @@ public class PersonController {
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/person/name")
+    public ResponseEntity<?> getPerson() {
+
+        FhirContext ctx = FhirContext.forR4();
+        String serverBase = "http://localhost:8080/fhir";
+        IGenericClient client = ctx.newRestfulGenericClient(serverBase);
+        IParser parser = ctx.newJsonParser();
+
+            Bundle response = client.search()
+                    .forResource(Patient.class)
+                    .where(Patient.FAMILY.matches().values("deo"))
+                    .returnBundle(Bundle.class)
+                    .execute();
+
+            System.out.println("Found " + response.getTotal());
+
+            for (BundleEntryComponent entry : response.getEntry()) {
+			
+                String jsonEncoded = parser.encodeResourceToString(entry.getResource());
+    
+                System.out.println("-------------------------------------------------------------------");
+                System.out.println(jsonEncoded);
+                
+                Patient patient = FhirContext.forR4().newJsonParser().parseResource(Patient.class, jsonEncoded);
+                
+                System.out.println(patient.getName().toString());
+                System.out.println(patient.getGender());
+                System.out.println(patient.getBirthDate());
+                System.out.println(patient.getAddress());
+                
+                String formatedName = patient.getNameFirstRep().getGivenAsSingleString()+" "+patient.getNameFirstRep().getNameAsSingleString();
+                
+                String formatedAdderess = 
+                        patient.getAddressFirstRep().getCountry()+ ", "+
+                        patient.getAddressFirstRep().getCity()+ " "+
+                        "("+patient.getAddressFirstRep().getPostalCode()+")";
+    
+                String formatedID = patient.getIdentifierFirstRep().getValue();	
+                
+                
+                // result+= "ID: "+formatedID+System.lineSeparator();
+                // result+= "Active: "+patient.getActive()+System.lineSeparator();
+                // result+= "Name: "+formatedName+System.lineSeparator();
+                // result+= "Birth Date: "+patient.getBirthDate()+System.lineSeparator();
+                // result+= "Aderess: "+formatedAdderess+System.lineSeparator();	
+            }
+        
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/emr/moh/modal/Person.java
+++ src/main/java/com/emr/moh/modal/Person.java
@@ -1,18 +1,22 @@
 package com.emr.moh.modal;
 
+import java.sql.Date;
+
 import lombok.Data;
 
 @Data
 public class Person {
 
-    private Long id;
+    private String id;
     private String fname;
     private String lname;
+    private String otherNames;
     private String email;
     private String mobileNo;
     private String state;
     private String city;
     private String postalCode;
-    private String gender;
-    
+    private String gender; 
+    private Boolean deceased;
+    private Date birthDate;
 }
